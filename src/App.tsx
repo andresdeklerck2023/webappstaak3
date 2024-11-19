47cbb20bf08c73e13c8af916b9235f535bc0cb11
andresdeklerck2023 <andresdeklerck@gmail.com>
2024-11-19 20:25:08 +0100

    app.tsx nogmaalsnogmaals  aangepast

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div style={appStyle} className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-const backgroundColor = process.env.BACKGROUND_COLOR;
+const backgroundColor = process.env.REACT_APP_BG_COLOR;
 
 const appStyle = {
     backgroundColor,
@@ -33,6 +12,14 @@ const appStyle = {
     alignItems: 'center',
 };
 
-
+const App = () => (
+    <div style={appStyle}>
+        <h1>Hello, Azure Static Web Apps!</h1>
+    </div>
+);
 
 export default App;
+
+
+
+
